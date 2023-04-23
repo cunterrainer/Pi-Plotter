4021ea8f3e274e8f8ca78f0eb5f435dbea7e88e5
pyvyx <pyvyx@web.de>
2023-04-23 15:51:17 +0200

    FactorialTable will compute the factorials itself and no longer relies on a FactorialTableData file

--- Pi-Plotter/src/FactorialTable.h
+++ Pi-Plotter/src/FactorialTable.h
@@ -1,65 +1,60 @@
 #pragma once
 #include <vector>
-#include <string>
-#include <fstream>
 
 #include "gmp.h"
 
 #include "Log.h"
+#include "Profiler.h"
 #include "ProgressBar.h"
-//#include "FactorialTableData.h"
 
 class FactorialTable
 {
 private:
     std::vector<MP_INT> m_Table;
 public:
-    FactorialTable()
+    inline FactorialTable(const FactorialTable&) = delete;
+    inline FactorialTable(FactorialTable&&) = delete;
+    inline FactorialTable& operator=(const FactorialTable&) = delete;
+    inline FactorialTable&& operator=(FactorialTable&&) = delete;
+
+    inline FactorialTable()
     {
-        std::ifstream data("FactorialTableData");
-        if (!data.is_open())
-        {
-            Err << "Failed to open [FactorialTableData] hence no factorial table was created" << Endl;
-            return;
-        }
+        constexpr unsigned long limit = 100000;
+        Log << "Creating factorial table up to " << limit << Endl;
+        Profiler::Start();
+        MP_INT prev;
+        mpz_init(&prev);
+        mpz_set_ui(&prev, 1);
     
-        size_t i = 0;
-        std::string line;
-        std::getline(data, line);
-        size_t lines = std::stoul(line);
-    
-        Log << "Creating factorial table from precomputed file" << Endl;
+        m_Table.reserve(limit);
         ProgressBarInit();
-        while (std::getline(data, line))
+        for (unsigned long i = 1; i <= limit; ++i)
         {
-            ProgressBar((float)i, (float)lines-1);
-            MP_INT val;
-            mpz_init2(&val, (mp_bitcnt_t)line.size());
-            mpz_set_str(&val, line.c_str(), 62);
-            m_Table.push_back(val);
-            ++i;
+            if(i % 1000 == 0)
+                ProgressBar((float)i, (float)limit);
+
+            MP_INT tmp;
+            mpz_init2(&tmp, 28);
+            mpz_set(&tmp, &prev);
+            m_Table.push_back(tmp);
+
+            mpz_mul_ui(&prev, &prev, i);
         }
-        Log << "Created factorial table with size: " << m_Table.size() - 1 << Endl;
+        mpz_clear(&prev);
+        Profiler::End();
+        Log << "Created factorial table" << Endl;
+        Log << "Execution time: " << Profiler::Average(Profiler::Conversion::Seconds) << " sec(s)" << Endl;
     }
 
-    //FactorialTable()
-    //{
-    //    Log << "Creating factorial table from precompiled binary" << Endl;
-    //    ProgressBarInit();
-    //    for (size_t i = 0; i < FactorialTableData.size(); ++i)
-    //    {
-    //        ProgressBar((float)i, (float)FactorialTableData.size() - 1);
-    //        MP_INT val;
-    //        mpz_init2(&val, (mp_bitcnt_t)FactorialTableData[i].size());
-    //        mpz_set_str(&val, FactorialTableData[i].data(), 62);
-    //        m_Table.push_back(val);
-    //    }
-    //    Log << "Created factorial table with size: " << m_Table.size() - 1 << Endl;
-    //}
+    inline ~FactorialTable()
+    {
+        for (MP_INT& mp : m_Table)
+            mpz_clear(&mp);
+    }
 
-    inline MP_INT& operator[](size_t index)
+    inline const MP_INT& operator[](size_t index) const
     {
         return m_Table[index];
     }
 };
-inline FactorialTable FactTable;
+inline const FactorialTable FactTable;
